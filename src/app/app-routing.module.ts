e2281b677a28b895a33a2dc2cd02cb46d12da966
jisson thomas <jisson.thomas.m@gmail.com>
2022-07-12 11:47:51 +0530

    added tab layout for create update survey

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateSurveyFormComponent } from './create-survey-form/create-survey-form.component';
 import { CreateUpdateSurveyComponent } from './create-update-survey/create-update-survey.component';
 
-const routes: Routes = [{path:'create',component:CreateUpdateSurveyComponent}];
+const routes: Routes = [{path:'create',component:CreateUpdateSurveyComponent},{path:'survey',component:CreateSurveyFormComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatTabsModule} from '@angular/material/tabs';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateUpdateSurveyComponent } from './create-update-survey/create-update-survey.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CreateSurveyFormComponent } from './create-survey-form/create-survey-form.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CreateUpdateSurveyComponent
+    CreateUpdateSurveyComponent,
+    CreateSurveyFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
